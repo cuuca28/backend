80492d4579d3d38b65358717d4fc329c2c825c39
cuuca28 <romibetan7@gmail.com>
2023-10-23 18:21:50 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const ProductManager = require('./ProductManager');
+
+const app = express();
+const port = 3000; 
+
+app.use(express.json());
+
+const productManager = new ProductManager(); /
+
+app.get('/products', async (req, res) => {
+  try {
+    const { limit } = req.query;
+    const products = await productManager.getProducts(limit);
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: 'Error al obtener productos' });
+  }
+});
+
+app.get('/products/:pid', async (req, res) => {
+  try {
+    const { pid } = req.params;
+    const product = await productManager.getProductById(pid);
+    
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ error: 'Producto no encontrado' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Error al obtener el producto' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Servidor Express en ejecución en el puerto ${port}`);
+});
---
+++ src/ProductManager.js
@@ -0,0 +1,107 @@
+const fs = require('fs');
+
+class ProductManager {
+    constructor(path) {
+        this.path = path;
+    }
+
+    addProduct(product) {
+        const products = this.getProducts();
+        if (!products) {
+            product.id = this.getNextId(products);
+            products.push(product);
+        } else {
+            product.id = 1;
+            products.push(product);
+        }
+        this.saveProducts(products);
+    }
+
+    getProducts() {
+        try {
+            const data = fs.readFileSync(this.path, 'utf8');
+            return JSON.parse(data);
+        } catch (err) {
+            return [];
+        }
+    }
+
+    getProductById(productId) {
+        const products = this.getProducts();
+        return products.find(product => product.id === productId);
+    }
+
+    updateProduct(productId, updatedProduct) {
+        const products = this.getProducts();
+        const index = products.findIndex(product => product.id === productId);
+        if (index !== -1) {
+            products[index] = updatedProduct;
+            this.saveProducts(products);
+            return true;
+        }
+        return false;
+    }
+
+    deleteProduct(productId) {
+        const products = this.getProducts();
+        const index = products.findIndex(product => product.id === productId);
+        if (index !== -1) {
+            products.splice(index, 1);
+            this.saveProducts(products);
+            return true;
+        }
+        return false;
+    }
+
+    saveProducts(products) {
+        const data = JSON.stringify(products, null, 4);
+        fs.writeFileSync(this.path, data, 'utf8');
+    }
+
+    getNextId(products) {
+        if (products.length === 0) {
+            return 1;
+        }
+        const maxId = Math.max(...products.map(product => product.id));
+        return maxId + 1;
+    }
+}
+
+const productManager = new ProductManager('products.json');
+
+const newProduct = {
+    title: 'Hojillas',
+    description: 'Hojillas para quemar el cigarrillo de forma controlada',
+    price: 100,
+    code: 'P1',
+    stock: 100,
+  };
+productManager.addProduct(newProduct);
+
+productManager.addProduct({
+    title: 'Tabaco sabor miel 50g',
+    description: 'Tabaco con el mejor saborizante a miel natural',
+    price: 250,
+    code: 'P2',
+    stock: 50,
+  });
+
+const allProducts = productManager.getProducts();
+console.log(allProducts);
+
+const productId = 1;
+const product = productManager.getProductById(productId);
+console.log(product);
+
+const updatedProduct = {
+    id: productId,
+    title: 'Producto Actualizado',
+    description: 'Descripción actualizada',
+    price: 12.99,
+    code: 'XYZ789',
+    stock: 50
+};
+productManager.updateProduct(productId, updatedProduct);
+
+const productIdToDelete = 1;
+productManager.deleteProduct(productIdToDelete);
